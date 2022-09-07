19567588828367ddb75e7a1fbc4bfd556dd2ec88
ValenSaZu <111878177+ValenSaZu@users.noreply.github.com>
2022-09-06 22:48:01 -0500

    Add files via upload

--- ejercicios cap 2.1/ej_2.28.cpp
+++ ejercicios cap 2.1/ej_2.28.cpp
@@ -4,11 +4,20 @@
 
 int main(){
     using namespace std;
-    int n1{0},n2{0},n3{0},n4{0};
+    int numero{0};
     cout<<"Ingrese cuatro numeros: ";
-    cin>>n1>>n2>>n3>>n4;
+    cin>>numero;
 
-    cout<<n4<<"  "<<n3<<"  "<<n2<<"  "<<n1;
+    if (numero >= 1000){
+        int n1 = numero/1000;
+        numero  = numero - n1*1000;
+        int n2 = numero/100;
+        numero = numero - n2*100;
+        int n3 = numero/10;
+        numero = numero - n3*10;
+        int n4 = numero;
+        cout<<n4<<"  "<<n3<<"  "<<n2<<"  "<<n1;
+    }
 
 return 0;
 }
