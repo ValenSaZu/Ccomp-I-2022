a11ab5e3a9fac6fd5032bf400c25345c6c902746
ValenSaZu <111878177+ValenSaZu@users.noreply.github.com>
2022-09-20 07:54:19 -0500

    Update ej_4.cpp

--- ejercicios euler/ej_4.cpp
+++ ejercicios euler/ej_4.cpp
@@ -1,25 +1,26 @@
 #include <iostream>
 
-int encontrarPalindromo(int num){
+int encontrarPalindromo(int n){
     int palin=0;
+    int num = n;
     while(num>0){
         palin=palin * 10 + (num % 10);
         num = num / 10;
         }
     
-    if (num==palin){
-        return num;
+    if (n==palin){
+        return n;
     }else{
         return 1;
     }
 }
 
 int palinMayor(){
-    for(int a=99*99; a>=10*10; a=a-1){
+    for(int a=999*999; a>=100*100; a=a-1){
         if (encontrarPalindromo(a) != 1){
-            for(int b=99; b>=10; b=b-1){
+            for(int b=999; b>=100; b=b-1){
                 int c = a/b;
-                if (c>=10 && c<=99){
+                if (c>=100 && c<=999 && a%b==0){
                     return a;
                     break;
                 }
