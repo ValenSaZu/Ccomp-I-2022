5d04fb47703117c2b195fb7f5ae2596f41639af2
ValenSaZu <111878177+ValenSaZu@users.noreply.github.com>
2022-09-21 07:05:32 -0500

    Update ej_5.cpp

--- ejercicios euler/ej_5.cpp
+++ ejercicios euler/ej_5.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-
+#include <math.h>
+using namespace std;
 int Primo(int num){
     int sum{0};
     for (int counter{2};counter<num; ++counter){
         if(num==2){
-            return 2
+            return 2;
         }
         if (num%counter==0){
             return 0;}
@@ -13,17 +14,26 @@ int Primo(int num){
 }
 
 int multiplica(int n,int x){
-    int num = n
-    int result{1}
-    while (x<=n){
-        if (n/x)
+    int mult{1};
+    int result{0};
+    while (mult<=n){
+        mult*=x;
+        result+=1;
     }
+    return result;
 }
 
 int smallest_multiple(int n){
+    int result{1};
     for(int counter{2}; counter<=n; ++counter){
         if (Primo(counter)!=0){
-            
+            int x =multiplica(n,counter);
+            result*=pow(counter,x);
         }
     }
+    return result;
+}
+
+int main(){
+    cout<<smallest_multiple(10);
 }
