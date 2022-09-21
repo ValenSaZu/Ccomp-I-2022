dcf76d6ccb2db79702b0042a4734d0195d3d6ab6
ValenSaZu <111878177+ValenSaZu@users.noreply.github.com>
2022-09-21 06:01:15 -0500

    Add files via upload

---
+++ ejercicios euler/ej_5.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int Primo(int num){
+    int sum{0};
+    for (int counter{2};counter<num; ++counter){
+        if(num==2){
+            return 2
+        }
+        if (num%counter==0){
+            return 0;}
+    }
+    return num;
+}
+
+int multiplica(int n,int x){
+    int num = n
+    int result{1}
+    while (x<=n){
+        if (n/x)
+    }
+}
+
+int smallest_multiple(int n){
+    for(int counter{2}; counter<=n; ++counter){
+        if (Primo(counter)!=0){
+            
+        }
+    }
+}
