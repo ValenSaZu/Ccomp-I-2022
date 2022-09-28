ab78ad1e85fedecd6966628d776302cb2530cae5
ValenSaZu <111878177+ValenSaZu@users.noreply.github.com>
2022-09-28 07:30:00 -0500

    Add files via upload

---
+++ semana 4/main.cpp
@@ -0,0 +1,30 @@
+#include "class_point.h"
+#include <iostream>
+
+using namespace std;
+
+
+void imprimirPoint (Point p) {
+    cout << "("<<p.getX()<<", "<<p.getY()<<")"<<endl;
+}
+void modificar (int a, int b, Point &p){
+    p.setX(a);
+    p.setY(b);
+}
+
+void intercambio(int &a,int &b){
+    int other = a;
+    a = b;
+    b = other;
+}
+int main() {
+    
+    Point p1(5, 5);
+    imprimirPoint(p1);
+    modificar(9, 2, p1);
+    imprimirPoint(p1);
+    return 0;
+
+    int a = 10;
+    int b = 20;
+}
