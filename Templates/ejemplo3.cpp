8a19d70e806f783023e09aad153578dce3f1eab7
ValenSaZu <111878177+ValenSaZu@users.noreply.github.com>
2022-11-15 08:26:45 -0500

    Add files via upload

---
+++ Templates/ejemplo3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Point.h"
+
+using namespace std;
+
+int main(){
+
+    Point<float> fpoint(2.3, 6.5);
+
+    cout << fpoint.getX() << fpoint.getY() <<endl;
+    
+    return 0;
+}
---
+++ Templates/primer_clase.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/*template <typename T>
+T sum(const T a, const T b){
+    return a+b;
+}*/
+
+template <typename T, typename U>
+U sum(const T a, const U b){
+    return a + b;
+}
+
+int main(){
+
+    cout << sum<int>(1,5) <<endl;
+    cout << sum<double>(4.5, 6.7)<<endl;
+    cout << sum<string>("hola ", "mundo")<<endl;
+    cout << sum<int, float>(1, 2.5)<< endl;
+
+    return 0;
+}
